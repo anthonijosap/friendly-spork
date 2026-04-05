634ea978dbc357e7bbfc8b202bcf997d6466f63f
anthonijosap <anthonijosap2022@gmail.com>
2026-04-05 22:23:34 +0530

    lower, replacing escape

--- word2vec.py
+++ word2vec.py
@@ -2,3 +2,8 @@ file = open('word2vecdata.txt', 'r')
 lines = file.readlines()
 file.close()
 print(lines)
+
+for i in range(len(lines)):
+    lines[i] = lines[i].lower().replace('\n', '')
+
+print(lines)
