e988f71ecf9ecc0ee65dac7c7529a98aedf5038d
anthonijosap <anthonijosap2022@gmail.com>
2026-04-06 09:23:43 +0530

    stopwords removal

--- word2vec.py
+++ word2vec.py
@@ -7,3 +7,15 @@ for i in range(len(lines)):
     lines[i] = lines[i].lower().replace('\n', '')
 
 print(lines)
+
+stopwords = ['the', 'is', 'in', 'and', 'to', 'of','a', 'that', 'it', 'with', 
+             'as', 'for', 'was', 'on', 'are', 'by', 'this', 'be', 'or', 'from']
+
+filtered_lines = []
+
+for line in lines:
+    for word in line.split():
+        if word not in stopwords:
+            filtered_lines.append(word)
+
+print(filtered_lines)
