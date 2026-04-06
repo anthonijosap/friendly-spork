72d12c9b8ffd790e53911eeb187ce640832bcba9
anthonijosap <anthonijosap2022@gmail.com>
2026-04-06 09:32:20 +0530

    bigram

--- word2vec.py
+++ word2vec.py
@@ -14,8 +14,19 @@ stopwords = ['the', 'is', 'in', 'and', 'to', 'of','a', 'that', 'it', 'with',
 filtered_lines = []
 
 for line in lines:
+    temp = []
     for word in line.split():
         if word not in stopwords:
-            filtered_lines.append(word)
+            temp.append(word)
+    filtered_lines.append(temp)
 
 print(filtered_lines)
+
+bigram = []
+
+for line in filtered_lines:
+    for i in range(len(line)-1):
+        for j in range(i+1, len(line)):
+            bigram.append([line[i], line[j]])
+            bigram.append([line[j], line[i]])
+print(bigram)
